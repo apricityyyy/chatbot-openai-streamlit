86fdee451c4207f2aa5cbb1d05297ab5989af78e
Ilhama Novruzova <111725646+apricityyyy@users.noreply.github.com>
2023-12-20 20:46:26 +0400

    Update streamlit_chat_bot.py

--- streamlit_chat_bot.py
+++ streamlit_chat_bot.py
@@ -1,4 +1,4 @@
-import openai 
+import openai
 import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -6,7 +6,8 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot") if "messages" not in st.session_state:
+st.title("💬 Chatbot") 
+if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
